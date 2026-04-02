b949949093881680c6ce1c914d9bdb3992a3246d
HITEN900 <mohantyhiten579@gmail.com>
2026-04-02 12:42:53 +0530

    first commit

--- easybook/settings.py
+++ easybook/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'crispy_forms',
     'crispy_bootstrap5',
+    
     # 'django_filters',  # Disabled due to Django 6.0.3 compatibility
     
     # Authentication
